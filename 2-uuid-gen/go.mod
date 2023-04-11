82bd95671dc63b449d0c08ad6feb462832e1e551
MSkrzypietz <michael.skr97@gmail.com>
2023-04-11 20:55:46 +0200

    implement uuid generation

---
+++ 2-uuid-gen/go.mod
@@ -0,0 +1,8 @@
+module 2-uuid-gen
+
+go 1.20
+
+require (
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20230410034848-d1ba02cffac2
+	github.com/oklog/ulid/v2 v2.1.0
+)
---
+++ 2-uuid-gen/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	ulid "github.com/oklog/ulid/v2"
+)
+
+func main() {
+    n := maelstrom.NewNode()
+
+    n.Handle("generate", func(msg maelstrom.Message) error {
+        var body map[string]any
+        if err := json.Unmarshal(msg.Body, &body); err != nil {
+            return err
+        }
+
+        body["type"] = "generate_ok"
+        body["id"] = ulid.Make()
+
+        return n.Reply(msg, body)
+    })
+
+    if err := n.Run(); err != nil {
+        log.Fatal(err)
+    }
+}
